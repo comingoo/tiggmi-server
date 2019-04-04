7f63010494b07a0048818d34ef48c48238377844
Shesh Kumar Mishra <mishrakshesh14287@gmail.com>
2019-04-04 13:40:07 +0530

    #1 Setup Project and create db

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Faker\Factory as Faker;
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        User::truncate();//Empty all user record
+        $faker = Faker::create();
+        $password = Hash::make('123456');
+      
+        User::create([
+            'name' => 'Shesh',
+            'email' => 'mishrakshesh14287@gmail.com',
+            'password' => $password,
+            'mobile'=>'8318198224',
+            'isVerified'=>0
+        ]);
+    }
+}
